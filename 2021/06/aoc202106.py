5cf2683a8f4318e17560884c680391a389af28b5
psycho-baller <Ramim66809@gmail.com>
2021-12-10 23:28:36 +0400

    day 6 done

--- 2021/06/aoc202106.py
+++ 2021/06/aoc202106.py
@@ -3,18 +3,45 @@
 # Standard library imports
 import pathlib
 import sys
+import collections
 
 
 def parse(puzzle_input):
     """Parse input"""
+    return collections.Counter(int(x) for x in puzzle_input.strip().split(','))
+
+
+def main(data, days):
+    for _ in range(days):
+        # all 0's will turn to a 6 and an 8
+        not_data = collections.Counter({6: data[0], 8: data[0]})
+        # all numbers in the data greater than 0 will be reduced by 1
+        not_data.update({time-1: counter for time,
+                        counter in data.items() if time > 0})
+        data = not_data
+    return sum(data.values())
+    #####    inefficient way    #####
+    #day = 0                        #
+    #while day < days:              #
+    #    for t in range(len(data)): #
+    #        if data[t] == 0:       #
+    #            data[t] = 6        #
+    #            data.append(8)     #
+    #        else:                  #
+    #            data[t] -= 1       #
+    #    day += 1                   #
+    #return len(data)               #
+    #################################
 
 
 def part1(data):
     """Solve part 1"""
+    return main(data, 80)
 
 
 def part2(data):
     """Solve part 2"""
+    return main(data, 256)
 
 
 def solve(puzzle_input):
