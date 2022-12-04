3828d3676eead8e4aeb1844c48f4036cd74151df
Rami Maalouf <rami.rami@ucalgary.ca>
2022-12-03 23:04:26 -0700

    done day 4

--- 2022/04/1.py
+++ 2022/04/1.py
@@ -1,5 +1,24 @@
 from pathlib import Path
 
-# inp = Path('input.txt').read_text().splitlines()
-inp = Path('sample.txt').read_text().splitlines()
+inp = Path('input.txt').read_text().splitlines()
+# inp = Path('sample.txt').read_text().splitlines()
 
+count = 0
+for pair in inp:
+    # logic
+    # a-b,c-d
+    # we need a >= c and b <= d
+    # or a <= c and b >= d
+    
+    # we will parse the line
+    elf1, elf2 = pair.split(',')
+    a, b = elf1.split('-')
+    c, d = elf2.split('-')
+    
+    # convert to int
+    a, b, c, d = int(a), int(b), int(c), int(d)
+    
+    # logic time
+    if (a >= c and b <= d) or (a <= c and b >= d):
+        count += 1
+print(count)
--- 2022/04/2.py
+++ 2022/04/2.py
@@ -1,5 +1,25 @@
 from pathlib import Path
 
-# inp = Path('input.txt').read_text().splitlines()
-inp = Path('sample.txt').read_text().splitlines()
+inp = Path('input.txt').read_text().splitlines()
+# inp = Path('sample.txt').read_text().splitlines()
 
+count = 0
+for pair in inp:
+    # logic
+    # a-b,c-d
+    # elf1 overlaps if a >= c and a <= d or b >= c and b <= d
+    # elf2 overlaps if c >= a and c <= b or d >= a and d <= b
+    
+    
+    # we will parse the line
+    elf1, elf2 = pair.split(',')
+    a, b = elf1.split('-')
+    c, d = elf2.split('-')
+    
+    # convert to int
+    a, b, c, d = int(a), int(b), int(c), int(d)
+    
+    # logic time
+    if (( a >= c and a <= d) or (b >= c and b <= d)) or ((c >= a and c <= b) or (d >= a and d <= b)):
+        count += 1
+print(count)
