9ea4eff4a54448df29949bfb1e0adb90727162af
Rami Maalouf <rami.rami@ucalgary.ca>
2022-12-05 22:37:17 -0700

    day 6 done

--- 2022/06/1.py
+++ 2022/06/1.py
@@ -1,5 +1,11 @@
 from pathlib import Path
 
-# inp = Path('input.txt').read_text().splitlines()
-inp = Path('sample.txt').read_text().splitlines()
+inp = Path('input.txt').read_text()
+# inp = Path('sample.txt').read_text()
 
+for i in range(4,len(inp)):
+    sett = set(inp[i-4:i])
+    
+    if (len(sett) == 4):
+        print(i)
+        break
--- 2022/06/2.py
+++ 2022/06/2.py
@@ -1,5 +1,11 @@
 from pathlib import Path
 
-# inp = Path('input.txt').read_text().splitlines()
-inp = Path('sample.txt').read_text().splitlines()
-
+inp = Path('input.txt').read_text()
+# inp = Path('sample.txt').read_text()
+distinct_nums = 14
+for i in range(distinct_nums,len(inp)):
+    sett = set(inp[i-distinct_nums:i])
+    
+    if (len(sett) == distinct_nums):
+        print(i)
+        break
