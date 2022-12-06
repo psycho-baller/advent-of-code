a0c101b7432ce045f4269d8da9e9ac9c4193aab9
Rami Maalouf <rami.rami@ucalgary.ca>
2022-12-05 21:58:43 -0700

    half done with half of day 5, YT time

--- 2022/05/1.py
+++ 2022/05/1.py
@@ -1,5 +1,20 @@
 from pathlib import Path
 
 # inp = Path('input.txt').read_text().splitlines()
-inp = Path('sample.txt').read_text().splitlines()
+stack_data, actions = Path('sample.txt').read_text().split("\n\n")
+stack_data = stack_data.splitlines()
+stacks = {}
+numbers = stack_data.pop()
 
+# parse stacks
+for i in numbers:
+    if i != " ":
+        stacks[int(i)] = []
+    
+for line in reversed(stack_data):
+    for i in range(1, len(line), 4):
+        if line[i] != " ":
+            key = (i-1)//4 + 1
+            stacks[key].append(line[i])
+
+# 
